5d1433d8796c3bae2075c7124b00355b41922270
SONY <SONY@192.168.0.7>
2014-12-16 00:51:07 +0900

    -

--- clien-stream/bin/startup.bat
+++ clien-stream/bin/startup.bat
@@ -1,4 +1,4 @@
 set CURRENT_PATH=%~dp0
-set CLASSPATH=%CURRENT_PATH%\..\WEB-INF\lib\*;%CURRENT_PATH%\PresentationChat-0.0.1-SNAPSHOT.jar
+set CLASSPATH=%CURRENT_PATH%\..\WEB-INF\lib\*;%CURRENT_PATH%\clien-stream-0.0.1-SNAPSHOT.jar
 
-"%JAVA_HOME%\bin\java" -cp %CLASSPATH% org.lasti.PresentationChat.App
+"%JAVA_HOME%\bin\java" -cp %CLASSPATH% org.jaeyo.clien_stream.App
