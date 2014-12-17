78d2e08094ba285bbba314380e0f1633f1c0945c
leejy <leejy@leejy-PC>
2014-12-17 18:17:55 +0900

    -

--- clien-stream/src/main/java/org/jaeyo/clien_stream/TODO.java
+++ clien-stream/src/main/java/org/jaeyo/clien_stream/TODO.java
@@ -2,4 +2,5 @@ package org.jaeyo.clien_stream;
 
 public class TODO {
 //	- bbsItem 수집할 때 title 옆에 replyCount 붙는거 어떻게 파싱하는지 확인 필요. 10초마다 parsing 할때 replyCount 다르면 해당 article 재수집 필요
+//	- web socket에서 받을때 정렬이 반대로 된 것 같다. 확인 필요
 } //class
