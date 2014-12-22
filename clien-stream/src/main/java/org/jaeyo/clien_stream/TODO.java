b11d5d16e15b91dd0fa123711b1a612845705348
SONY <SONY@192.168.0.7>
2014-12-23 00:51:39 +0900

    -

--- clien-stream/src/main/java/org/jaeyo/clien_stream/TODO.java
+++ clien-stream/src/main/java/org/jaeyo/clien_stream/TODO.java
@@ -2,7 +2,6 @@ package org.jaeyo.clien_stream;
 
 public class TODO {
 //	- reply count는 어떡하지
-//	- hit 의미없지 않나.. 뺄까.
 //	- view단 bbs 목록 예쁘게 꾸며야 함
-//	- fix된 item들을 쿠키에 저장
+//	- fix된 item들을 web storage에 저장
 } //class
